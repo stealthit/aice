c13d6ad9536d2e7f68c916ee5da55ee0bb5f29d5
ganachecell <bluegag@gmail.com>
2022-11-18 20:48:11 +0900

    add ready list page, ready detail tab1

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+$(".catg-menu li").on("mouseover",function () {
+  const tabId = $(this).attr("data-tab"); 
+  
+  $("#" + tabId).addClass("active").siblings().removeClass('active');
+})
+$(".tab-menu li").on("click",function () {
+  const tabId = $(this).attr("data-tab"); 
+  $(this).addClass('active').siblings().removeClass('active');  
+  $(".tab-menu img").removeClass('active');
+  $(this).children().first().addClass('active');
+  $("#" + tabId).addClass("active").siblings().removeClass('active');
+})
+
+$(".search-box input").on("focus",function(){
+  $(this).parent(".search-box").css('background', '#fff');
+  $(this).parent(".search-box").css('border-color', '#003594');
+  $(this).siblings(".ic-search").addClass("sel");
+})
+$(".search-box input").on("blur",function(){
+  $(this).parent(".search-box").css('background', '#f3f3f3');
+  $(this).parent(".search-box").css('border-color', '#f3f3f3');
+  $(this).siblings(".ic-search").removeClass('sel');
+})
+
+// 정렬박스 -최신순,조회순,인기순
+$(".sort-box .selected").on("click",function(){
+  $(".sort-box").toggleClass("open");
+})
+$(".optionList .optionItem").on("click",function(){
+  $(".sort-box .selected .selected-value")[0].innerText = $(this)[0].innerText;
+  $(this).addClass('sel').siblings().removeClass('sel');
+  $(".sort-box").removeClass("open");
+})
+
+// input delete button
+function searchInputDelete() {
+  const formSearchInput = document.querySelector('#search-text');
+  const formSearchClose = document.querySelector('.btn-input-delete');
+  
+  formSearchInput.addEventListener('keyup', () => {
+    formSearchClose.style.display =  (formSearchInput.value == '') ? "none":"flex" ;    
+  });
+  
+  formSearchClose.addEventListener('click',(e) => {
+    e.currentTarget.style.display = "none";
+  })
+}
+
+
