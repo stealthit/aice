f960391f240fe4442b3265ce6023ce2adfb2e72b
ganachecell <bluegag@gmail.com>
2022-11-21 22:05:18 +0900

    update circleProgress function

--- scripts/script.js
+++ scripts/script.js
@@ -51,7 +51,8 @@ function searchInputDelete() {
 }
 
 // circle progress bar
-function circleProgress(bar, per) {
+function circleProgress(RADIUS, bar, per) {
+  var CIRCUMFERENCE = 2 * Math.PI * RADIUS;    
   var progress = per / 100;
   var dashoffset = CIRCUMFERENCE * (1 - progress);    
   bar.style.strokeDashoffset = dashoffset;
